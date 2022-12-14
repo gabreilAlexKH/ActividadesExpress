e298f8c7e74f103fe20fe1f8fe89e2d16831c9b8
Cliente CONTROL-PC <gabamald@espol.edu.ec>
2022-12-14 10:09:51 -0500

    Admin layout

--- admin/routes/index.js
+++ admin/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/photos', function(req, res, next) {
+  res.render('fotos', { title: 'Fotos' });
+})
+
 module.exports = router;
